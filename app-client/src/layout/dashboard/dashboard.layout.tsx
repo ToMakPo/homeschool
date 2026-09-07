654c96e93ca650a2becee5637d7ce67a69c8fca7
Makai Post <postmakai@gmail.com>
2026-09-07 07:23:20 -0700

    refactor: update DashboardLayout structure for improved semantic HTML

--- app-client/src/layout/dashboard/dashboard.layout.tsx
+++ app-client/src/layout/dashboard/dashboard.layout.tsx
@@ -2,10 +2,10 @@ import './dashboard.styles.scss'
 
 const DashboardLayout = () => {
 	return (
-		<div id='dashboard-layout'>
-			<div id='dashboard-layout-header'>
+		<div id='dashboard-layout' className='main-content'>
+			<header className='header'>
 				<h1>Dashboard</h1>
-			</div>
+			</header>
 		</div>
 	)
 }
